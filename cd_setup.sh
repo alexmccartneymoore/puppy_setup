64639e7710f09d71fbef8ca697251922d20f39d1
Alex McCartney <alexmccartney@protonmail.com>
2024-10-22 11:17:40 +0000

    first commit

---
+++ cd_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# get the config files using curl
+# curl -O address_of_configs
+
+# cp or move the config files 
+# mv ./.jwmrc-theme ~/.jwm/.jwmrc-theme
+# mv ./.jwmrc-tray ~/.jwmrc-tray
+
+# restart jwm
+jwm -restart
---
+++ install.md
@@ -0,0 +1,11 @@
+# Install to Wyse 3040
+
+### Usb frugal install to disk
+Boot the usb
+Select frugal install
+Proceed with the frugal install
+Run the install setup script
+```
+./cd_setup.sh
+```
+
